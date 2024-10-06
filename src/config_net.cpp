4b59c50e688529b4e05dd8120e948f0e7f88ec78
Ivo Julca <ivojulca@hotmail.com>
2024-10-06 17:37:43 -0500

    Fix style

--- src/config_net.cpp
+++ src/config_net.cpp
@@ -130,7 +130,7 @@ CNetConfig::CNetConfig(CConfig& CFG)
 
   m_EnableTCPWrapUDP             = CFG.GetBool("net.tcp_extensions.udp_tunnel.enabled", true);
   m_EnableTCPScanUDP             = CFG.GetBool("net.tcp_extensions.udp_scan.enabled", true);
-  m_VLANEnabled                   = CFG.GetBool("net.tcp_extensions.gproxy.vlan.enabled", true);
+  m_VLANEnabled                  = CFG.GetBool("net.tcp_extensions.gproxy.vlan.enabled", true);
   m_VLANPort                     = CFG.GetUint16("net.tcp_extensions.gproxy.vlan.port", onlyHostPort.value_or(6112u));
 
   m_ReconnectWaitTime            = CFG.GetUint8("net.tcp_extensions.gproxy.reconnect_wait", 5);
