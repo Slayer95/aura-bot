Slayer95/aura-bot

commit 4317270f380f357d507164458f0659fd9b6be70e

Support M16 server

src/config/config_realm.cpp
@@ -45,6 +45,7 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CNetConfig* NetConfig)
 
     // Inheritable
     m_Enabled(true),
+    m_LocaleShort({69, 80, 115, 101}), // esPE, reversed
     m_AutoRegister(false),
     m_UserNameCaseSensitive(false),
     m_PassWordCaseSensitive(false),
@@ -62,6 +63,26 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CNetConfig* NetConfig)
   m_CountryShort           = CFG.GetString(m_CFGKeyPrefix + "country_short", "PER");
   m_Country                = CFG.GetString(m_CFGKeyPrefix + "country", "Peru");
   m_Locale                 = CFG.GetString(m_CFGKeyPrefix + "locale", "system");
+
+  string localeShort = CFG.GetString(m_CFGKeyPrefix + "locale_short", 4, 4, "esPE"); // esPE reversed
+  copy_n(localeShort.rbegin(), 4, m_LocaleShort.begin());
+
+  {
+    bool localeError = CFG.GetErrorLast();
+    if (!localeError) {
+      if (
+        m_LocaleShort[0] < 0x41 || m_LocaleShort[1] < 0x41 || m_LocaleShort[2] < 0x61 || m_LocaleShort[3] < 0x61 ||
+        m_LocaleShort[0] > 0x5a || m_LocaleShort[1] > 0x5a || m_LocaleShort[2] > 0x7a || m_LocaleShort[3] > 0x7a
+      )
+      {
+        localeError = true;
+      }
+    }
+    if (localeError) {
+      Print("[CONFIG] Error - invalid value provided for <" + m_CFGKeyPrefix + "locale_short> - must provide a valid pair of ISO 639-1, ISO 3166 alpha-2 identifiers (e.g. enUS)");
+      CFG.SetFailed();
+    }
+  }
 
   if (m_Locale == "system") {
     m_LocaleID = 10250;
@@ -116,6 +137,7 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CNetConfig* NetConfig)
   m_PassWord               = CFG.GetString(m_CFGKeyPrefix + "password", m_PassWord);
 
   m_AuthUseCustomVersion   = CFG.GetBool(m_CFGKeyPrefix + "auth_custom", false);
+  m_AuthIgnoreVersionError = CFG.GetBool(m_CFGKeyPrefix + "auth_ignore_version_error", false);
   m_AuthPasswordHashType   = CFG.GetStringIndex(m_CFGKeyPrefix + "auth_password_hash_type", {"pvpgn", "battle.net"}, REALM_AUTH_PVPGN);
 
   m_AuthWar3Version        = CFG.GetMaybeUint8(m_CFGKeyPrefix + "auth_game_version");
@@ -207,6 +229,7 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CRealmConfig* nRootConfig, uint8_t nSer
     m_Country(nRootConfig->m_Country),
     m_Locale(nRootConfig->m_Locale),
     m_LocaleID(nRootConfig->m_LocaleID),
+    m_LocaleShort(nRootConfig->m_LocaleShort),
 
     m_PrivateCmdToken(nRootConfig->m_PrivateCmdToken),
     m_BroadcastCmdToken(nRootConfig->m_BroadcastCmdToken),
@@ -235,6 +258,7 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CRealmConfig* nRootConfig, uint8_t nSer
     m_PassWord(nRootConfig->m_PassWord),
 
     m_AuthUseCustomVersion(nRootConfig->m_AuthUseCustomVersion),
+    m_AuthIgnoreVersionError(nRootConfig->m_AuthIgnoreVersionError),
     m_AuthPasswordHashType(nRootConfig->m_AuthPasswordHashType),
 
     m_AuthWar3Version(nRootConfig->m_AuthWar3Version),
@@ -278,7 +302,25 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CRealmConfig* nRootConfig, uint8_t nSer
 
   m_CountryShort           = CFG.GetString(m_CFGKeyPrefix + "country_short", m_CountryShort);
   m_Country                = CFG.GetString(m_CFGKeyPrefix + "country", m_Country);
-  m_Locale                 = CFG.GetString(m_CFGKeyPrefix + "locale", m_Locale);
+
+  if (CFG.Exists(m_CFGKeyPrefix + "locale_short")) {
+    string localeShort       = CFG.GetString(m_CFGKeyPrefix + "locale_short", 4, 4, "esPE");
+    bool localeError = CFG.GetErrorLast();
+    if (!localeError) {
+      copy_n(localeShort.rbegin(), 4, m_LocaleShort.begin());
+      if (
+        m_LocaleShort[0] < 0x41 || m_LocaleShort[1] < 0x41 || m_LocaleShort[2] < 0x61 || m_LocaleShort[3] < 0x61 ||
+        m_LocaleShort[0] > 0x5a || m_LocaleShort[1] > 0x5a || m_LocaleShort[2] > 0x7a || m_LocaleShort[3] > 0x7a
+      )
+      {
+        localeError = true;
+      }
+    }
+    if (localeError) {
+      Print("[CONFIG] Error - invalid value provided for <" + m_CFGKeyPrefix + "locale_short> - must provide a valid pair of ISO 639-1, ISO 3166 alpha-2 identifiers (e.g. enUS)");
+      CFG.SetFailed();
+    }
+  }
 
   if (m_Locale == "system") {
     m_LocaleID = 10250;
@@ -335,6 +377,7 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CRealmConfig* nRootConfig, uint8_t nSer
   if (!m_PassWordCaseSensitive) m_PassWord = ToLowerCase(m_PassWord);
 
   m_AuthUseCustomVersion   = CFG.GetBool(m_CFGKeyPrefix + "auth_custom", m_AuthUseCustomVersion);
+  m_AuthIgnoreVersionError = CFG.GetBool(m_CFGKeyPrefix + "auth_ignore_version_error", m_AuthIgnoreVersionError);
   m_AuthPasswordHashType   = CFG.GetStringIndex(m_CFGKeyPrefix + "auth_password_hash_type", {"pvpgn", "battle.net"}, m_AuthPasswordHashType);
 
   // These are optional, since they can be figured out with bncsutil.

config-example.ini
@@ -379,6 +379,28 @@ realm_21.map_transfers.max_size = 3072
 realm_21.commands.custom_host.permissions = verified
 realm_21.enabled = yes
 
+realm_25.host_name = 3d83b79312a03679207d5dbd06de14fe.m16.me
+realm_25.server_port = 5004
+realm_25.unique_name = M16
+realm_25.username = 
+realm_25.password = 
+realm_25.sudo_users = 
+realm_25.admins = 
+realm_25.auth_game_version = 28
+realm_25.auth_exe_version = 0 5 28 1
+realm_25.auth_exe_version_hash = 201 63 116 96
+realm_25.auth_ignore_version_error = yes
+realm_25.locale = 1042
+realm_25.locale_short = koKR
+realm_25.country_short = KO
+realm_25.country = South Korea
+realm_25.map_transfers.max_size = 8192
+realm_25.first_channel = The Void
+realm_25.commands.custom_host.permissions = verified
+realm_25.enabled = yes
+realm_25.flood.wrap = 80
+realm_25.flood.max_size = 256
+
 ############################################
 # UDP CONFIGURATION ("Local Area Network") #
 ############################################

src/config/config_realm.h
@@ -72,10 +72,11 @@ struct CRealmConfig
 
   // Inheritable
 
-  std::string m_CountryShort;                    // 2-3 letter country code for pvpgn servers
+  std::string m_CountryShort;                    // ISO 3166 alpha-3 country code
   std::string m_Country;                         // country name
   std::string m_Locale;                          // locale used: numeric or "system"
   uint32_t m_LocaleID;                           // see: http://msdn.microsoft.com/en-us/library/0h88fahh%28VS.85%29.aspx
+  std::array<uint8_t, 4> m_LocaleShort;          // language (ISO 639-1 concatenated with ISO 3166 alpha-2) - reversed internally
 
   std::string m_PrivateCmdToken;                 // a symbol prefix to identify commands and send a private reply
   std::string m_BroadcastCmdToken;               // a symbol prefix to identify commands and send the reply to everyone
@@ -105,9 +106,10 @@ struct CRealmConfig
   std::string m_PassWord;                        //
 
   bool m_AuthUseCustomVersion;
+  bool m_AuthIgnoreVersionError;
   uint8_t m_AuthPasswordHashType;                         // pvpgn or battle.net
 
-  std::optional<uint8_t> m_AuthWar3Version;                  // WC3 minor version
+  std::optional<uint8_t> m_AuthWar3Version;                   // WC3 minor version
   std::optional<std::vector<uint8_t>> m_AuthExeVersion;       // 4 bytes: WC3 version as {patch, minor, major, build}
   std::optional<std::vector<uint8_t>> m_AuthExeVersionHash;   // 4 bytes
   std::string m_AuthExeInfo;                                  // filename.exe dd/MM/yy hh::mm:ss size

src/net.cpp
@@ -248,20 +248,16 @@ bool CNet::GetIsOutgoingThrottled(const NetworkHost& host) const
     return true;
   }
 
-  {
-    const auto it = m_OutgoingThrottles.find(host);
-    if (it == m_OutgoingThrottles.end()) {
-      return false;
-    }
-    const TimedUint8& throttled = it->second;
-    if (throttled.second == 0) {
-      return false;
-    }
-    const int64_t dueTime = throttled.first + ((int64_t) NET_BASE_RECONNECT_DELAY << (int64_t)throttled.second);
-    return GetTime() < dueTime;
+  const auto it = m_OutgoingThrottles.find(host);
+  if (it == m_OutgoingThrottles.end()) {
+    return false;
   }
-
-  return false;
+  const TimedUint8& throttled = it->second;
+  if (throttled.second == 0) {
+    return false;
+  }
+  const int64_t dueTime = throttled.first + ((int64_t) NET_BASE_RECONNECT_DELAY << (int64_t)throttled.second);
+  return GetTime() < dueTime;
 }
 
 void CNet::ResetOutgoingThrottled(const NetworkHost& host)

src/protocol/bnet_protocol.cpp
@@ -747,31 +747,30 @@ namespace BNETProtocol
     return packet;
   }
 
-  vector<uint8_t> SEND_SID_AUTH_INFO(uint8_t ver, uint32_t localeID, const string& CountryShort, const string& country)
+  vector<uint8_t> SEND_SID_AUTH_INFO(uint8_t ver, uint32_t localeID, const array<uint8_t, 4>& localeShort, const string& countryShort, const string& country)
   {
     const uint8_t ProtocolID[]    = {0, 0, 0, 0};
     const uint8_t PlatformID[]    = {54, 56, 88, 73};              // "IX86"
     const uint8_t ProductID_TFT[] = {80, 88, 51, 87};              // "W3XP"
     const uint8_t Version[]       = {ver, 0, 0, 0};
-    const uint8_t Language[]      = {83, 85, 110, 101};            // "enUS"
     const uint8_t LocalIP[]       = {127, 0, 0, 1};
     const uint8_t TimeZoneBias[]  = {60, 0, 0, 0};                 // 60 minutes (GMT +0100) but this is probably -0100
 
     vector<uint8_t> packet;
-    packet.push_back(BNETProtocol::Magic::BNET_HEADER);                        // BNET header constant
-    packet.push_back(BNETProtocol::Magic::AUTH_INFO);         // SID_AUTH_INFO
+    packet.push_back(BNETProtocol::Magic::BNET_HEADER);             // BNET header constant
+    packet.push_back(BNETProtocol::Magic::AUTH_INFO);              // SID_AUTH_INFO
     packet.push_back(0);                                           // packet length will be assigned later
     packet.push_back(0);                                           // packet length will be assigned later
     AppendByteArray(packet, ProtocolID, 4);                        // Protocol ID
     AppendByteArray(packet, PlatformID, 4);                        // Platform ID
     AppendByteArray(packet, ProductID_TFT, 4);                     // Product ID (TFT)
     AppendByteArray(packet, Version, 4);                           // Version
-    AppendByteArray(packet, Language, 4);                          // Language (hardcoded as enUS to ensure battle.net sends the bot messages in English)
+    AppendByteArrayFast(packet, localeShort);                      // Reverse language (ISO 639-1 concatenated with ISO 3166 alpha-2, and reversed)
     AppendByteArray(packet, LocalIP, 4);                           // Local IP for NAT compatibility
     AppendByteArray(packet, TimeZoneBias, 4);                      // Time Zone Bias
     AppendByteArray(packet, localeID, false);                      // Locale ID
     AppendByteArray(packet, localeID, false);                      // Language ID (copying the locale ID should be sufficient since we don't care about sublanguages)
-    AppendByteArrayFast(packet, CountryShort);                     // Country Abbreviation
+    AppendByteArrayFast(packet, countryShort);                     // Country Abbreviation
     AppendByteArrayFast(packet, country);                          // Country
     AssignLength(packet);
 

src/protocol/bnet_protocol.h
@@ -252,7 +252,7 @@ namespace BNETProtocol
   [[nodiscard]] std::vector<uint8_t> SEND_SID_PING(const std::array<uint8_t, 4>& pingValue);
   [[nodiscard]] std::vector<uint8_t> SEND_SID_LOGONRESPONSE(const std::vector<uint8_t>& clientToken, const std::vector<uint8_t>& serverToken, const std::vector<uint8_t>& passwordHash, const std::string& accountName);
   [[nodiscard]] std::vector<uint8_t> SEND_SID_NETGAMEPORT(uint16_t serverPort);
-  [[nodiscard]] std::vector<uint8_t> SEND_SID_AUTH_INFO(uint8_t ver, uint32_t localeID, const std::string& CountryShort, const std::string& country);
+  [[nodiscard]] std::vector<uint8_t> SEND_SID_AUTH_INFO(uint8_t ver, uint32_t localeID, const std::array<uint8_t, 4>& localeShort, const std::string& countryShort, const std::string& country);
   [[nodiscard]] std::vector<uint8_t> SEND_SID_AUTH_CHECK(const std::array<uint8_t, 4>& clientToken, const std::array<uint8_t, 4>& exeVersion, const std::array<uint8_t, 4>& exeVersionHash, const std::vector<uint8_t>& keyInfoROC, const std::vector<uint8_t>& keyInfoTFT, const std::string& exeInfo, const std::string& keyOwnerName);
   [[nodiscard]] std::vector<uint8_t> SEND_SID_AUTH_ACCOUNTLOGON(const std::array<uint8_t, 32>& clientPublicKey, const std::string& accountName);
   [[nodiscard]] std::vector<uint8_t> SEND_SID_AUTH_ACCOUNTLOGONPROOF(const std::array<uint8_t, 20>& clientPasswordProof);

src/realm.cpp
@@ -339,7 +339,7 @@ void CRealm::Update(void* fd, void* send_fd)
 
             case BNETProtocol::Magic::AUTH_CHECK: {
               BNETProtocol::AuthCheckResult checkResult = BNETProtocol::RECEIVE_SID_AUTH_CHECK(Data);
-              if (checkResult.state == BNETProtocol::KeyResult::GOOD)
+              if (m_Config.m_AuthIgnoreVersionError || checkResult.state == BNETProtocol::KeyResult::GOOD)
               {
                 // cd keys accepted
                 DPRINT_IF(LOG_LEVEL_TRACE, GetLogPrefix() + "version OK")
@@ -601,7 +601,7 @@ void CRealm::Update(void* fd, void* send_fd)
       PRINT_IF(LOG_LEVEL_DEBUG, GetLogPrefix() + "connected to [" + m_HostName + "]")
       SendAuth(BNETProtocol::SEND_PROTOCOL_INITIALIZE_SELECTOR());
       m_GameVersion = m_Config.m_AuthWar3Version.has_value() ? m_Config.m_AuthWar3Version.value() : m_Aura->m_GameVersion;
-      SendAuth(BNETProtocol::SEND_SID_AUTH_INFO(m_GameVersion, m_Config.m_LocaleID, m_Config.m_CountryShort, m_Config.m_Country));
+      SendAuth(BNETProtocol::SEND_SID_AUTH_INFO(m_GameVersion, m_Config.m_LocaleID, m_Config.m_LocaleShort, m_Config.m_CountryShort, m_Config.m_Country));
       m_Socket->DoSend(static_cast<fd_set*>(send_fd));
       m_LastGameListTime       = Time;
       return;

commit 789c1648c6237c8526be5291ca26b336f2383348

Fix <realm_N.locale>, broken in 4317270f

src/config/config_realm.cpp
@@ -302,6 +302,7 @@ CRealmConfig::CRealmConfig(CConfig& CFG, CRealmConfig* nRootConfig, uint8_t nSer
 
   m_CountryShort           = CFG.GetString(m_CFGKeyPrefix + "country_short", m_CountryShort);
   m_Country                = CFG.GetString(m_CFGKeyPrefix + "country", m_Country);
+  m_Locale                 = CFG.GetString(m_CFGKeyPrefix + "locale", m_Locale);
 
   if (CFG.Exists(m_CFGKeyPrefix + "locale_short")) {
     string localeShort       = CFG.GetString(m_CFGKeyPrefix + "locale_short", 4, 4, "esPE");