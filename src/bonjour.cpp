5b69069f89a39c1a28785f35a0b786256b0546aa
Ivo Julca <ivojulca@hotmail.com>
2025-04-25 02:37:26 -0500

    Fix indentation

--- src/bonjour.cpp
+++ src/bonjour.cpp
@@ -10,23 +10,23 @@ CBonjour::CBonjour(string interfs)
  : m_Client(nullptr),
    m_Admin(nullptr)
 {
-	//DNSServiceRegister(&m_Client, 0, kDNSServiceInterfaceIndexAny, "_blizzard", "_udp.local", "", NULL, (uint16_t)6112, 0, "", nullptr, NULL);
-	interf = interfs;
+  //DNSServiceRegister(&m_Client, 0, kDNSServiceInterfaceIndexAny, "_blizzard", "_udp.local", "", NULL, (uint16_t)6112, 0, "", nullptr, NULL);
+  interf = interfs;
 #ifndef DISABLE_BONJOUR  
-	int err = DNSServiceCreateConnection(&m_Admin);
-	if (err) Print("[MDNS] DNSServiceCreateConnection 1 failed: " + to_string(err) + "\n");
-	err = DNSServiceCreateConnection(&m_Client);
-	if (err) Print("[MDNS] DNSServiceCreateConnection 0 failed: " + to_string(err) + "\n");
+  int err = DNSServiceCreateConnection(&m_Admin);
+  if (err) Print("[MDNS] DNSServiceCreateConnection 1 failed: " + to_string(err) + "\n");
+  err = DNSServiceCreateConnection(&m_Client);
+  if (err) Print("[MDNS] DNSServiceCreateConnection 0 failed: " + to_string(err) + "\n");
 
-	Print("[MDNS] DNS registration finished");
+  Print("[MDNS] DNS registration finished");
 #endif
 }
 
 CBonjour::~CBonjour()
 {
 #ifndef DISABLE_BONJOUR
-	if (m_Client) DNSServiceRefDeallocate(m_Client);
-	if (m_Admin) DNSServiceRefDeallocate(m_Admin);
+  if (m_Client) DNSServiceRefDeallocate(m_Client);
+  if (m_Admin) DNSServiceRefDeallocate(m_Admin);
 #endif
 }
 
@@ -34,11 +34,11 @@ CBonjour::~CBonjour()
 optional<pair<DNSServiceRef, DNSRecordRef>> CBonjour::FindGame(shared_ptr<CGame> game)
 {
   optional<pair<DNSServiceRef, DNSRecordRef>> result;
-	for (auto it = games.begin(); it != games.end(); it++) {
-		if (get<1>(*it).lock() == game) {
-			get<2>(*it) = GetTime();
+  for (auto it = games.begin(); it != games.end(); it++) {
+    if (get<1>(*it).lock() == game) {
+      get<2>(*it) = GetTime();
       result = make_pair(get<0>(*it), get<3>(*it));
-		}
+    }
   }
   return result;
 }
@@ -49,10 +49,10 @@ void CBonjour::BroadcastGameInner(shared_ptr<CGame> game, const string& gameName
   uint32_t slotsTotal = game->GetNumSlots();
   uint16_t hostPort = game->GetHostPortForDiscoveryInfo(AF_INET);
 
-	string players_num = ToDecString(slotsOff); // slots taken ???
-	string players_max = to_string(slotsTotal);
-	string secret = to_string(game->GetEntryKey());
-	string time = to_string(game->GetUptime()); // creation time uint32_t ???
+  string players_num = ToDecString(slotsOff); // slots taken ???
+  string players_max = to_string(slotsTotal);
+  string secret = to_string(game->GetEntryKey());
+  string time = to_string(game->GetUptime()); // creation time uint32_t ???
 
   vector<uint8_t> statInfo;
   AppendByteArray(statInfo, game->GetGameFlags(), false);
@@ -69,10 +69,10 @@ void CBonjour::BroadcastGameInner(shared_ptr<CGame> game, const string& gameName
     return;
   }
 
-	vector<uint8_t> encodedStatString = EncodeStatString(statInfo);
+  vector<uint8_t> encodedStatString = EncodeStatString(statInfo);
 
   vector<uint8_t> game_data_d;
-	if (gameVersion.second >= 32) {
+  if (gameVersion.second >= 32) {
     // try this for games with short names on pre32?
     const std::array<uint8_t, 4> magicNumber = {0x01, 0x20, 0x43, 0x00}; // 1.32.6700 ??
     AppendByteArray(game_data_d, gameName, true);
@@ -82,7 +82,7 @@ void CBonjour::BroadcastGameInner(shared_ptr<CGame> game, const string& gameName
     AppendByteArray(game_data_d, (uint32_t)slotsTotal, false);
     AppendByteArrayFast(game_data_d, magicNumber);
     AppendByteArray(game_data_d, hostPort, false);
-	} else {
+  } else {
     const uint32_t One = 1;
     AppendByteArray(game_data_d, One, false);
     AppendByteArray(game_data_d, gameName, true);
@@ -94,31 +94,31 @@ void CBonjour::BroadcastGameInner(shared_ptr<CGame> game, const string& gameName
     AppendByteArray(game_data_d, hostPort, false);
   }
 
-	string game_data = Base64::Encode(reinterpret_cast<unsigned char*>(game_data_d.data()), game_data_d.size(), false);
+  string game_data = Base64::Encode(reinterpret_cast<unsigned char*>(game_data_d.data()), game_data_d.size(), false);
 
-	string w66;
-	if (gameVersion.second != 30) {
-		w66	= string("\x0A") + ((char)gameName.size()) + gameName + string("\x10\0", 2) +
-		"\x1A" + (char)(15 + players_num.size()) + "\x0A\x0Bplayers_num\x12" + (char)players_num.size() + players_num +
-		"\x1A" + (char)(9 + gameName.size()) + "\x0A\x05_name\x12" + (char)gameName.size() + gameName +
-		"\x1A" + (char)(15 + players_max.size()) + "\x0A\x0Bplayers_max\x12" + (char)players_max.size() + players_max + 
-		"\x1A" + (char)(20 + time.size()) + "\x0A\x10game_create_time\x12" + (char)time.size() + time +
-		"\x1A\x0A\x0A\x05_type\x12\x01\x31" + "\x1A\x0D\x0A\x08_subtype\x12\x01\x30" +
-		"\x1A" + (char)(15 + secret.size()) + "\x0A\x0Bgame_secret\x12" + (char)secret.size() + secret +
-		"\x1A" + (char)(14 + game_data.size()) + "\x01\x0A\x09game_data\x12" + (char)game_data.size() + "\x01" + game_data +
-		"\x1A\x0C\x0A\x07game_id\x12\x01" + ((gameVersion.second >= 32) ? "1": "2") +
-		"\x1A\x0B\x0A\x06_flags\x12\x01\x30";
-	} else {
-		w66 = string("\x0A") + ((char)gameName.size()) + gameName + string("\x10\0", 2) +
-		"\x1A" + (char)(15 + secret.size()) + "\x0A\x0Bgame_secret\x12" + (char)secret.size() + secret +
-		"\x1A\x0A\x0A\x05_type\x12\x01\x31" + "\x1A\x0D\x0A\x08_subtype\x12\x01\x30" +
-		"\x1A\x0C\x0A\x07game_id\x12\x01" + "1" +
-		"\x1A" + (char)(9 + gameName.size()) + "\x0A\x05_name\x12" + (char)gameName.size() + gameName +
-		"\x1A" + (char)(15 + players_max.size()) + "\x0A\x0Bplayers_max\x12" + (char)players_max.size() + players_max +
-		"\x1A\x0B\x0A\x06_flags\x12\x01\x30" +
-		"\x1A" + (char)(15 + players_num.size()) + "\x0A\x0Bplayers_num\x12" + (char)players_num.size() + players_num +
-		"\x1A" + (char)(20 + time.size()) + "\x0A\x10game_create_time\x12" + (char)time.size() + time +
-		"\x1A" + (char)(14 + game_data.size()) + "\x01\x0A\x09game_data\x12" + (char)game_data.size() + "\x01" + game_data;
+  string w66;
+  if (gameVersion.second != 30) {
+    w66  = string("\x0A") + ((char)gameName.size()) + gameName + string("\x10\0", 2) +
+    "\x1A" + (char)(15 + players_num.size()) + "\x0A\x0Bplayers_num\x12" + (char)players_num.size() + players_num +
+    "\x1A" + (char)(9 + gameName.size()) + "\x0A\x05_name\x12" + (char)gameName.size() + gameName +
+    "\x1A" + (char)(15 + players_max.size()) + "\x0A\x0Bplayers_max\x12" + (char)players_max.size() + players_max + 
+    "\x1A" + (char)(20 + time.size()) + "\x0A\x10game_create_time\x12" + (char)time.size() + time +
+    "\x1A\x0A\x0A\x05_type\x12\x01\x31" + "\x1A\x0D\x0A\x08_subtype\x12\x01\x30" +
+    "\x1A" + (char)(15 + secret.size()) + "\x0A\x0Bgame_secret\x12" + (char)secret.size() + secret +
+    "\x1A" + (char)(14 + game_data.size()) + "\x01\x0A\x09game_data\x12" + (char)game_data.size() + "\x01" + game_data +
+    "\x1A\x0C\x0A\x07game_id\x12\x01" + ((gameVersion.second >= 32) ? "1": "2") +
+    "\x1A\x0B\x0A\x06_flags\x12\x01\x30";
+  } else {
+    w66 = string("\x0A") + ((char)gameName.size()) + gameName + string("\x10\0", 2) +
+    "\x1A" + (char)(15 + secret.size()) + "\x0A\x0Bgame_secret\x12" + (char)secret.size() + secret +
+    "\x1A\x0A\x0A\x05_type\x12\x01\x31" + "\x1A\x0D\x0A\x08_subtype\x12\x01\x30" +
+    "\x1A\x0C\x0A\x07game_id\x12\x01" + "1" +
+    "\x1A" + (char)(9 + gameName.size()) + "\x0A\x05_name\x12" + (char)gameName.size() + gameName +
+    "\x1A" + (char)(15 + players_max.size()) + "\x0A\x0Bplayers_max\x12" + (char)players_max.size() + players_max +
+    "\x1A\x0B\x0A\x06_flags\x12\x01\x30" +
+    "\x1A" + (char)(15 + players_num.size()) + "\x0A\x0Bplayers_num\x12" + (char)players_num.size() + players_num +
+    "\x1A" + (char)(20 + time.size()) + "\x0A\x10game_create_time\x12" + (char)time.size() + time +
+    "\x1A" + (char)(14 + game_data.size()) + "\x01\x0A\x09game_data\x12" + (char)game_data.size() + "\x01" + game_data;
   }
 
   {
@@ -149,30 +149,30 @@ void CBonjour::BroadcastGame(shared_ptr<CGame> game, const Version& gameVersion)
     service1 = storedGame->first;
     record = storedGame->second;
   } else {
-		string tmp = string("_blizzard._udp") + (game->GetIsExpansion() ? ",_w3xp" : ",_war3") + ToHexString(gameVersion.second + 10000);
-		const char* temp = tmp.c_str();
-		
+    string tmp = string("_blizzard._udp") + (game->GetIsExpansion() ? ",_w3xp" : ",_war3") + ToHexString(gameVersion.second + 10000);
+    const char* temp = tmp.c_str();
+    
     service1 = m_Client;
     int err = DNSServiceRegister(
       &(storedGame->first), kDNSServiceFlagsShareConnection, kDNSServiceInterfaceIndexAny, gameName.c_str(),
       temp, "local", NULL, ntohs(8152), 0, NULL, nullptr, NULL
     );
-		if (err) {
-			Print("[MDNS] DNSServiceRegister 1 failed: " + to_string(err) + "\n");
-			return;
-		}
-	}
+    if (err) {
+      Print("[MDNS] DNSServiceRegister 1 failed: " + to_string(err) + "\n");
+      return;
+    }
+  }
 
   BroadcastGameInner(game, gameName, gameVersion, service1, record, isNew);
 }
 
 void CBonjour::StopBroadcastGame(shared_ptr<CGame> game)
 {
-	for (auto it = games.begin(); it != games.end(); it++) {
-		if (get<1>(*it).lock() == game) {
+  for (auto it = games.begin(); it != games.end(); it++) {
+    if (get<1>(*it).lock() == game) {
       DNSServiceRefDeallocate(get<0>(*it));
-			it = games.erase(it);
-		}
+      it = games.erase(it);
+    }
   }
 }
 
@@ -186,4 +186,3 @@ bool CBonjour::CheckLibrary()
   return CheckDynamicLibrary(PLATFORM_STRING("dnssd.so"), PLATFORM_STRING("Bonjour"));
 #endif
 }
-
--- src/bonjour.h
+++ src/bonjour.h
@@ -16,23 +16,23 @@
 class CBonjour
 {
 #ifndef DISABLE_BONJOUR  
-	std::vector<std::tuple<DNSServiceRef, std::weak_ptr<CGame>, uint32_t, DNSRecordRef>> games;
+  std::vector<std::tuple<DNSServiceRef, std::weak_ptr<CGame>, uint32_t, DNSRecordRef>> games;
 #endif
 public:
 #ifndef DISABLE_BONJOUR  
-	DNSServiceRef               m_Client;
-	DNSServiceRef               m_Admin;
+  DNSServiceRef               m_Client;
+  DNSServiceRef               m_Admin;
 #else
-	void*                       m_Client;
-	void*                       m_Admin;
+  void*                       m_Client;
+  void*                       m_Admin;
 #endif
-	std::string interf;
-	CBonjour(std::string interfs);
-	~CBonjour();
+  std::string interf;
+  CBonjour(std::string interfs);
+  ~CBonjour();
 
 #ifndef DISABLE_BONJOUR
   std::optional<std::pair<DNSServiceRef, DNSRecordRef>> FindGame(std::shared_ptr<CGame> nGame);
-	void BroadcastGame(std::shared_ptr<CGame> nGame, const Version& gameVersion);
+  void BroadcastGame(std::shared_ptr<CGame> nGame, const Version& gameVersion);
   void BroadcastGameInner(std::shared_ptr<CGame> nGame, const std::string& gameName, const Version& gameVersion, DNSServiceRef service, DNSRecordRef record, bool isNew);
   void StopBroadcastGame(std::shared_ptr<CGame> nGame);
 #endif
